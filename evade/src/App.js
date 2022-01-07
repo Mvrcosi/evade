b8b551787543a7fe2a374b2acd7aa6b0317fe07f
marcos323s <saucedomarcos1@yahoo.com>
2022-01-07 03:26:56 -0800

    initial cavnas set up

---
+++ evade/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Canvas from './components/Canvas/Canvas'
+import './App.css'
+const App = () => {
+    return (
+        <div>
+            <Canvas />
+        </div>
+    )
+}
+
+export default App
---
+++ evade/src/components/Canvas/Canvas.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Canvas.css'
+const Canvas = () => {
+    return (
+        <div>
+            <canvas></canvas>
+        </div>
+    )
+}
+export default Canvas
